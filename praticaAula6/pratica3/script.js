25358f809072be2327b814d1b0a04f8de1219451
Amanda Kado <amandakado.ak@gmail.com>
2024-09-23 21:37:49 -0300

    pratica3

---
+++ praticaAula6/pratica3/script.js
@@ -0,0 +1,18 @@
+let listaDesejos = [
+    'carro',
+    'apartamento',
+    'videogame'
+]
+
+let pessoa = {
+    nome: 'amanda',
+    idade: 23,
+    lista: listaDesejos
+}
+
+console.log(pessoa.nome='Joana');
+console.log(pessoa["lista"]);
+
+
+
+console.log(listaDesejos[2]);
