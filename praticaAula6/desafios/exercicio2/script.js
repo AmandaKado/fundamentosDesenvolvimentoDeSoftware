33928956d7a38abe1aabcc77652cd88ab46f408f
Amanda Kado <amandakado.ak@gmail.com>
2024-09-24 22:55:59 -0300

    exercicio 2

---
+++ praticaAula6/desafios/exercicio2/script.js
@@ -0,0 +1,10 @@
+const soma = document.getElementById('soma');
+const subtracao = document.getElementById('subtracao');
+const divisao = document.getElementById('divisao');
+const multiplicacao = document.getElementById('multiplicacao');
+const calcular = document.getElementById('calcular');
+
+let primeiroNumero = document.getElementById('numero1');
+let segundoNumero = document.getElementById('numero2');
+let resultado = document.getElementById('resultado');
+
