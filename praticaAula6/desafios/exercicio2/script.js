553b356f8b3266539e6d1b95ab519f536adb670d
Amanda Kado <amandakado.ak@gmail.com>
2024-09-24 23:05:04 -0300

    termianr depois

--- praticaAula6/desafios/exercicio2/script.js
+++ praticaAula6/desafios/exercicio2/script.js
@@ -1,4 +1,5 @@
 const soma = document.getElementById('soma');
+const selecao = document.querySelector('select');
 const subtracao = document.getElementById('subtracao');
 const divisao = document.getElementById('divisao');
 const multiplicacao = document.getElementById('multiplicacao');
@@ -8,3 +9,23 @@ let primeiroNumero = document.getElementById('numero1');
 let segundoNumero = document.getElementById('numero2');
 let resultado = document.getElementById('resultado');
 
+calcular.addEventListener("click", calculo());
+
+function somando() {
+    resultado = parseInt(primeiroNumero) + parseInt(segundoNumero);
+    div.innerHTML = resultado;
+}
+
+
+
+function calculo() {
+    if(selecao.soma) {
+        somando();
+    } else if(subtracao) {
+        subtraindo();
+    } else if(multiplicacao) {
+        multiplicando();
+    } else {
+        dividindo();
+    }
+}
